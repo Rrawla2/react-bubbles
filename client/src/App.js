17cdc7dbe676d1be1be0f6911475d40446d044c2
Kristi Gribble <kgribble4083@gmail.com>
2020-02-23 21:29:16 -0600

    Set up protected route, login form and authorization token, rendering bubbles.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
+import { BubbleContext } from "./context/BubbleContext";
 import "./styles.scss";
 
 function App() {
+  const [color, setColor] = useState([{
+    color: "",
+    code: {
+      hex: ""
+    },
+    id: ""
+  }]);
+
   return (
     <Router>
+    <BubbleContext.Provider value={{ color, setColor }}>
       <div className="App">
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <ProtectedRoute exact path="/protected" component={BubblePage}/>
       </div>
+      </BubbleContext.Provider>
     </Router>
   );
 }
--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/colors")
+      .then(response => {
+        console.log("colors: ", response)
+        setColorList(response.data)
+      })
+      .catch(err => console.log("Error", err))
+  }, [])
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
